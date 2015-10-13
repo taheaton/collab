a895bbb531e912142e78aa4d3674da6b7ca9c33d
Thomas Heaton <thomasaheaton@gmail.com>
2015-10-13 16:47:23 -0400

    javascript in

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,30 @@
 
 (function () {
 
-  console.log('It Works!');
+  var url = 'http://json-data.herokuapp.com/forms';
+
+  //fetch data and do something
+  var promise = $.getJSON(url);
+
+  promise.then(function (response) {
+    doSomething(response);
+  });
+
+  var genericInput = function genericInput(obj) {
+    var template = '\n    <div class="text-input">\n    <input value="" type="' + obj.type + '"placeholder="' + obj.label + '" id="' + obj.id + '">\n    <i class="fa ' + obj.icon + '"></i>\n    </div>\n    \n    ';
+    return template;
+  };
+
+  var doSomething = function doSomething(arr) {
+
+    _.each(arr, function (item) {
+
+      var htmlBlock;
+
+      if (item.type === 'text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea' || item.type === 'select' || item.options[0] === 'select') {
+        htmlBlock = genericInput(item);
+      }
+      $('form').append(htmlBlock);
+    });
+  };
 })();
--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
 (function () {
 
-  let formURL = 'http://json-data.herokuapp.com/forms';
-  var templateString = $('#registrationForm').text();
+  let url = 'http://json-data.herokuapp.com/forms';
 
-  var templateFunction = _.template(templateString);
+//fetch data and do something
+  let promise = $.getJSON(url);
 
-  $.ajax({
-    url: formURL,
-    dataType: 'jsonp',
-    method: 'get'
-  }).then (function (form) {
+  promise.then( function (response) {
+    doSomething(response);
+
+  });
+
+  let genericInput = function (obj) {
+    let template = `
+    <div class="text-input">
+    <input value="" type="${ obj.type }"placeholder="${ obj.label }" id="${obj.id }">
+    <i class="fa ${ obj.icon }"></i>
+    </div>
     
-    _.each(form.results, function (item) {
-      console.log(item);
-      var itemHTML = templateFunction(item);
-    
-      $('body').append(itemHTML)
+    `;
+    return template;
+  }
 
+let doSomething = function (arr) {
+
+    _.each(arr, function (item) {
+      
+      var htmlBlock;
+
+      if (item.type ==='text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea' || item.type === 'select' || item.options[0] === 'select') {
+        htmlBlock = genericInput(item);
+      }
+      $('form').append(htmlBlock);
     });
+  };
 
-}());
+  }());
